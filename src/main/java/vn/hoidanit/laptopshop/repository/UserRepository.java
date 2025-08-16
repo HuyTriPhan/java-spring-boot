5360b8c77e7eb03a6be676d9075f39e94e1fb575
Phan Huy Tri <huytriphan2k3@gmail.com>
2025-08-16 20:57:57 +0700

    New update

---
+++ src/main/java/vn/hoidanit/laptopshop/repository/UserRepository.java
@@ -0,0 +1,23 @@
+package vn.hoidanit.laptopshop.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import vn.hoidanit.laptopshop.domain.User;
+
+//crud: create, read, update, delete
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User save(User eric);
+
+    void deleteById(long id);
+
+    List<User> findOneByEmail(String email);
+
+    List<User> findAll();
+
+    User findById(long id); // null
+}
